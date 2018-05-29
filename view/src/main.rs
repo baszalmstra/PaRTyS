5bd817151f3fa08c81ca3feec0c2cfcd2c3a1f0b
Bas Zalmstra <zalmstra.bas@gmail.com>
2018-05-29 21:14:58 +0200

    restart

--- view/src/main.rs
+++ view/src/main.rs
@@ -1,39 +1,39 @@
-extern crate piston_window;
-extern crate opengl_graphics;
-extern crate cgmath;
-extern crate num_traits;
-extern crate time;
+extern crate ggez;
+use ggez::*;
+use ggez::graphics::{DrawMode, Point2};
 
-use piston_window::*;
-use opengl_graphics::GlGraphics;
+struct MainState {
+    pos_x: f32,
+}
 
-mod app;
-mod state;
-mod game;
-
-fn main() {
-    let mut window: PistonWindow = WindowSettings::new("PaRTyS", [1280, 800])
-        .exit_on_esc(true)
-        .build()
-        .unwrap();
-
-    let mut app = app::App::new();
-
-    app.load();
-
-    let mut gl = GlGraphics::new(OpenGL::V3_2);
-
-    while let Some(e) = window.next() {
-        if let Some(ref args) = e.render_args() {
-            app.render(args, &mut gl);
-        }
-
-        if let Some(ref args) = e.update_args() {
-            app.update(args);
-        }
-
-        // if let Some(ref args) = e.press_args() {
-        //     //app.key_press
-        // }
+impl MainState {
+    fn new(_ctx: &mut Context) -> GameResult<MainState> {
+        let s = MainState { pos_x: 0.0 };
+        Ok(s)
     }
+}
+
+impl event::EventHandler for MainState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        self.pos_x = self.pos_x % 800.0 + 1.0;
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx);
+        graphics::circle(ctx,
+                         DrawMode::Fill,
+                         Point2::new(self.pos_x, 380.0),
+                         100.0,
+                         2.0)?;
+        graphics::present(ctx);
+        Ok(())
+    }
+}
+
+fn main() -> GameResult<()> {
+    let c = conf::Conf::new();
+    let ctx = &mut Context::load_from_conf("super_simple", "ggez", c)?;
+    let state = &mut MainState::new(ctx)?;
+    event::run(ctx, state)
 }
