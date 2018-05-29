7e72b4684d869e50255ef3df3ca3e452bd679755
Bas Zalmstra <zalmstra.bas@gmail.com>
2018-05-29 22:09:33 +0200

    feat: Updated software, running piston example again

--- view/src/main.rs
+++ view/src/main.rs
@@ -1,39 +1,83 @@
-extern crate ggez;
-use ggez::*;
-use ggez::graphics::{DrawMode, Point2};
+extern crate piston;
+extern crate graphics;
+extern crate glutin_window;
+extern crate opengl_graphics;
+extern crate vecmath;
 
-struct MainState {
-    pos_x: f32,
+use piston::window::WindowSettings;
+use piston::event_loop::*;
+use piston::input::*;
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{ GlGraphics, OpenGL };
+
+
+pub struct App {
+    gl: GlGraphics, // OpenGL drawing backend.
+    rotation: f64   // Rotation for the square.
 }
 
-impl MainState {
-    fn new(_ctx: &mut Context) -> GameResult<MainState> {
-        let s = MainState { pos_x: 0.0 };
-        Ok(s)
+impl App {
+    fn render(&mut self, args: &RenderArgs) {
+        use graphics::*;
+
+        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+        const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+
+        let square = rectangle::square(0.0, 0.0, 50.0);
+        let rotation = self.rotation;
+        let (x, y) = ((args.width / 2) as f64,
+                      (args.height / 2) as f64);
+
+        self.gl.draw(args.viewport(), |c, gl| {
+            // Clear the screen.
+            clear(GREEN, gl);
+
+            let transform = c.transform.trans(x, y)
+                .rot_rad(rotation)
+                .trans(-25.0, -25.0);
+
+            // Draw a box rotating around the middle of the screen.
+            rectangle(RED, square, transform, gl);
+        });
+    }
+
+    fn update(&mut self, args: &UpdateArgs) {
+        // Rotate 2 radians per second.
+        self.rotation += 2.0 * args.dt;
     }
 }
 
-impl event::EventHandler for MainState {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
-        self.pos_x = self.pos_x % 800.0 + 1.0;
-        Ok(())
-    }
+fn main() {
+    let opengl = OpenGL::V3_2;
+    let mut window: Window = WindowSettings::new("PaRTyS", [1280, 800])
+        .exit_on_esc(true)
+        .opengl(opengl)
+        .build()
+        .unwrap();
 
-    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
-        graphics::clear(ctx);
-        graphics::circle(ctx,
-                         DrawMode::Fill,
-                         Point2::new(self.pos_x, 380.0),
-                         100.0,
-                         2.0)?;
-        graphics::present(ctx);
-        Ok(())
-    }
-}
+    let mut events = Events::new(EventSettings::new());
 
-fn main() -> GameResult<()> {
-    let c = conf::Conf::new();
-    let ctx = &mut Context::load_from_conf("super_simple", "ggez", c)?;
-    let state = &mut MainState::new(ctx)?;
-    event::run(ctx, state)
+
+    let mut gl = GlGraphics::new(OpenGL::V3_2);
+
+    // Create a new game and run it.
+    let mut app = App {
+        gl: GlGraphics::new(opengl),
+        rotation: 0.0
+    };
+
+    while let Some(e) = events.next(&mut window) {
+
+        if let Some(ref args) = e.render_args() {
+            app.render(args);
+        }
+
+        if let Some(ref args) = e.update_args() {
+            app.update(args);
+        }
+
+        // if let Some(ref args) = e.press_args() {
+        //     //app.key_press
+        // }
+    }
 }
